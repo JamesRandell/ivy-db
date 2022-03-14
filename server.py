979650a3bd17818cbb526932299985625ec2910d
James Randell <jamesrandell@me.com>
2022-03-14 16:06:59 +0000

    d

--- server.py
+++ server.py
@@ -54,4 +54,4 @@ class keyspace(Resource, base):
 api.add_resource(keyspace, '/<string:keyspace>')
 
 if __name__ == '__main__':
-     app.run(host='localhost', port='5000',ssl_context='adhoc')
+     app.run(port='5000',ssl_context='adhoc')
